8231cc1f0235dd15624486dcd0dcb0e5ce24d0f4
Alena <56043757+Elaine-still-alive@users.noreply.github.com>
2020-01-21 21:49:13 +0300

    :boom:

---
+++ Calc/Calc.cpp
@@ -0,0 +1,20 @@
+// Calc.cpp: главный файл проекта.
+
+#include "stdafx.h"
+#include "FFirst.h"
+
+using namespace Calc;
+
+
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Включение визуальных эффектов Windows XP до создания каких-либо элементов управления
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Создание главного окна и его запуск
+	Application::Run(gcnew Form1());
+	return 0;
+}
